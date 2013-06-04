7d34f54841672f651f0d3241da808470135c1316
Chris Dent <chris.dent@gmail.com>
2013-06-04 11:57:19 +0100

    Add failing test for #1
    and remove useless example test

---
+++ test/test_web_304.py
@@ -0,0 +1,75 @@
+
+import httplib2
+from wsgi_intercept import httplib2_intercept
+import wsgi_intercept
+from tiddlyweb.web.serve import load_app
+
+from tiddlyweb.config import config
+
+from tiddlyweb.model.bag import Bag
+from tiddlyweb.model.tiddler import Tiddler
+
+from tiddlywebplugins.utils import get_store
+
+import random
+import shutil
+import string
+
+
+RELEVANT_HEADERS = ['cache-control', 'etag', 'vary', 'last-modified']
+
+
+def setup_module(module):
+    # cleanup
+    try:
+        shutil.rmtree('store')
+    except OSError:
+        pass
+
+    # establish web server
+    app = load_app()
+    def app_fn():
+        return app
+    httplib2_intercept.install()
+    wsgi_intercept.add_wsgi_intercept('our_test_domain', 8001, app_fn)
+
+    module.store = get_store(config)
+    module.http = httplib2.Http()
+
+
+def _random_name(length=5):
+    return ''.join(random.choice(string.lowercase) for i in range(length))
+
+
+def test_single_tiddler():
+    bag = Bag(_random_name())
+    store.put(bag)
+
+    tiddler = Tiddler(_random_name(), bag.name)
+    tiddler.text = _random_name(10)
+    store.put(tiddler)
+
+    uri = 'http://our_test_domain:8001/bags/%s/tiddlers/%s' % (
+            tiddler.bag, tiddler.title)
+
+    _get_entity(uri)
+
+
+def _get_entity(uri):
+    response, content = http.request(uri, method='GET')
+
+    assert response['status'] == '200', content
+    response_200 = response
+
+    etag = response['etag']
+
+    response, content = http.request(uri, method='GET',
+            headers={'If-None-Match': etag})
+
+    assert response['status'] == '304', content
+    response_304 = response
+
+    for header in RELEVANT_HEADERS:
+        if header in response_200:
+            assert header in response_304
+            assert response_200[header] == response_304[header]
